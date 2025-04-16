8c57b72e99f3eeb1b450a61598304fbc3268686a
Sadat Shahriar <sadat@gakkmediabd.com>
2025-04-16 12:50:20 +0600

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,57 +1,67 @@
-let userConfig = undefined;
-try {
-  // try to import ESM first
-  userConfig = await import("./v0-user-next.config.mjs");
-} catch (e) {
-  try {
-    // fallback to CJS import
-    userConfig = await import("./v0-user-next.config");
-  } catch (innerError) {
-    // ignore error
-  }
-}
-
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-  basePath: "/portfolio-website",
-  assetPrefix: "/portfolio-website/",
   output: "export",
-  reactStrictMode: true,
-  trailingSlash: true, // Optional: Helps with GitHub Pages hosting
-  reactStrictMode: true,
+  distDir: "dist",
 };
 
-if (userConfig) {
-  // ESM imports will have a "default" property
-  const config = userConfig.default || userConfig;
-
-  for (const key in config) {
-    if (
-      typeof nextConfig[key] === "object" &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...config[key],
-      };
-    } else {
-      nextConfig[key] = config[key];
-    }
-  }
-}
-
 export default nextConfig;
+
+// let userConfig = undefined;
+// try {
+//   // try to import ESM first
+//   userConfig = await import("./v0-user-next.config.mjs");
+// } catch (e) {
+//   try {
+//     // fallback to CJS import
+//     userConfig = await import("./v0-user-next.config");
+//   } catch (innerError) {
+//     // ignore error
+//   }
+// }
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+//   experimental: {
+//     webpackBuildWorker: true,
+//     parallelServerBuildTraces: true,
+//     parallelServerCompiles: true,
+//   },
+//   // basePath: "/portfolio-website",
+//   // assetPrefix: "/portfolio-website/",
+//   // output: "export",
+//   reactStrictMode: true,
+//   trailingSlash: true, // Optional: Helps with GitHub Pages hosting
+//   reactStrictMode: true,
+// };
+
+// if (userConfig) {
+//   // ESM imports will have a "default" property
+//   const config = userConfig.default || userConfig;
+
+//   for (const key in config) {
+//     if (
+//       typeof nextConfig[key] === "object" &&
+//       !Array.isArray(nextConfig[key])
+//     ) {
+//       nextConfig[key] = {
+//         ...nextConfig[key],
+//         ...config[key],
+//       };
+//     } else {
+//       nextConfig[key] = config[key];
+//     }
+//   }
+// }
+
+// export default nextConfig;
